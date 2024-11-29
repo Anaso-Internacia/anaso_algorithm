f2d15f9f8751d5e61c86213f59d6b8fa5979f72c
Brandon Dyer <brandondyer64@gmail.com>
2024-11-29 12:47:17 -0700

    Basic starter algorithm

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# anaso_algorithm
+# Anaso Algorithm
+
 The Ana.so recommendation algorithm
+
+LICENSE: MIT
+
+You will notice that this is written in English and not Esperanto.
+That's because the industry standard, unfortunately, is monolingual.
+It would be considered bad practice, and only cause more confusion,
+to mix the two languages in the code. The website itself, however,
+is fully supported in Esperanto.
+
+Vi rimarkus ke tio ĉi estas skribita en la angla kaj ne Esperanto.
+Estas pro la industria normo, bedaŭrinde, kiu estas unulingva.
+Estus malbona praktiko, kaj nur kaŭzi konfuzon, miksi la du linvojn
+en la programa kodo. La retejo mem, tamen, estas tute subtenata
+en Esperanto.
+
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+//! The [Ana.so](https://ana.so) Algorithm
+
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+#![allow(clippy::pedantic)]
+
+/// Data used for scoring. Pass into [`score_post`].
+pub struct ScoringData {
+    /// Unix timestamp in seconds that the post was submitted.
+    time_posted: i64,
+    /// Number of likes the post has received in total.
+    likes: i64,
+}
+
+/// The static score value to be associated with a post.
+///
+/// This is made to be run on a post's data whenever
+/// a like is added or removed, and put in a score column
+/// on the post table.
+///
+/// As an example (though not the real code):
+///
+/// ```
+/// # use anaso_algorithm::*;
+/// # struct PostData {time_posted:i64,likes:i64}
+/// # fn query(s:&str,x:i64)->PostData{PostData{time_posted:0,likes:0}}
+/// # let id=0;
+/// // Get the relevant data for calculating the score
+/// let post: PostData = query("SELECT * FROM post WHERE id=?", id);
+///
+/// // Calculate the new score
+/// let score = score_post(ScoringData {
+///     time_posted: post.time_posted,
+///     likes: post.likes
+///     // ...
+/// });
+///
+/// // Update with score
+/// query("UPDATE post SET score=? WHERE id=?", id);
+/// ```
+///
+/// When getting posts for a particular page, they will be
+/// ordered by this value.
+///
+/// As an example (though not the real query):
+///
+/// ```sql
+/// SELECT * FROM post ORDER BY score LIMIT=20;
+/// ```
+pub fn score_post(data: ScoringData) -> i64 {
+    let score = (data.likes as f64).log10() as i64;
+    data.time_posted / 43200 + score
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {}
+}
